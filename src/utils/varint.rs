e27721d7439a48b8966000483355968f272d82f4
c2yz <c2yzawa@outlook.com>
2026-03-07 14:18:24 +0800

    fix error decode and feature test both

--- src/utils/varint.rs
+++ src/utils/varint.rs
@@ -16,53 +16,45 @@ pub enum VarIntError {
     #[error("io error: {0}")]
     Io(#[from] std::io::Error), // IO错误强转用的
 }
-
-fn encode(mut value: i32, buf: &mut impl Write) -> Result<(), VarIntError> {
+#[inline]
+pub fn encode(mut value: i32, buf: &mut impl Write) -> Result<(), VarIntError> {
     let mut value = value as u32; // 强转
-    for _ in 0..5 { // mc应该未来也不会有varlong吧，默认varint 5个字节最大
-        let mut byte = (value & 0x7F) as u8;
+    for i in 0..5 {
+        let byte = (value & 0x7F) as u8;
         value >>= 7;
-        if value != 0 {
-            byte |= 0x80;
-        }
+        let has_next = value != 0 && i < 4;
+        let byte = if has_next { byte | 0x80 } else { byte };
+
         buf.write_all(&[byte])?;
-        if value == 0 { // 正常结束了
+
+        if !has_next {
             return Ok(());
         }
     }
     Err(VarIntError::TooLarge)
 }
-
+#[inline]
 pub fn decode(reader: &mut impl Read) -> Result<i32, VarIntError> {
     let mut result = 0u32;
     let mut shift = 0;
-    let mut bytes_read = 0;
 
-    loop {
+    for _ in 0..5 {
         let mut buf = [0u8; 1];
         reader.read_exact(&mut buf)?;
         let byte = buf[0];
-        bytes_read += 1;
+
         let value = (byte & 0x7F) as u32;
         result |= value << shift;
 
-        // 如果最高位是0，说明这是最后一个字节
         if (byte & 0x80) == 0 {
-            if result > i32::MAX as u32 {
-                return Err(VarIntError::TooLarge);
-            }
             return Ok(result as i32);
         }
 
         shift += 7;
-
-        if bytes_read == 5 {
-            return Err(VarIntError::TooLarge);
-        }
     }
+    Err(VarIntError::TooLarge)
 }
-
-// 测试 cargo test -- --nocapture
+// 测试 cargo test test_encode --release -- --nocapture
 #[cfg(test)]
 mod tests {
     use rand::RngExt;
@@ -73,13 +65,13 @@ mod tests {
         println!("Encode varint test started");
         let mut rng = rand::rng();
         let mut buf = Vec::new();
-        let iterations = 10_000_000; // 一千万次
+        let iterations = 100_000_000; // 一亿次
 
         let start = std::time::Instant::now();
 
         for _ in 0..iterations {
             let n: i32 = rng.random();
-            encode(n, &mut buf).unwrap(); // 结果直接unwrap，出错说明逻辑问题
+            encode(n, &mut buf).expect("encode failed"); // 6666 Rust发力了
             buf.clear();
         }
 
@@ -90,5 +82,29 @@ mod tests {
 
         println!("Time: {:.3}s", secs);
         println!("Encode speed: {:.0}/s", speed);
+    }
+    #[test]
+    fn test_both() {
+        println!("Encode & Decode(Both) varint test started");
+        let mut rng = rand::rng();
+        let mut buf = Vec::new();
+        let iterations = 100_000_000; // 一亿次
+
+        let start = std::time::Instant::now();
+
+        for _ in 0..iterations {
+            let n: i32 = rng.random();
+            encode(n, &mut buf).expect("encode failed");
+            decode(&mut buf.as_slice()).expect("decode failed");
+            buf.clear();
+        }
+
+        let elapsed = start.elapsed();
+        let ns = elapsed.as_nanos();
+        let secs = ns as f64 / 1_000_000_000.0;
+        let speed = iterations as f64 / secs;
+
+        println!("Time: {:.3}s", secs);
+        println!("Encode & Decode speed: {:.0}/s", speed);
     }
 }
